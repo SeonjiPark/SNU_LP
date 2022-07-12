108086d91de4b16cc54491732f828a618f50f5df
Seonjipark <seonji2805@gmail.com>
2022-07-12 18:30:00 +0900

    first_push

---
+++ inference.py
@@ -0,0 +1,40 @@
+# YOLOv5 🚀 by Ultralytics, GPL-3.0 license
+"""
+Run inference on images, videos, directories, streams, etc.
+
+Usage - sources:
+    $ python path/to/detect.py --weights yolov5s.pt --source 0              # webcam
+                                                             img.jpg        # image
+                                                             vid.mp4        # video
+                                                             path/          # directory
+                                                             path/*.jpg     # glob
+                                                             'https://youtu.be/Zgi9g1ksQHc'  # YouTube
+"""
+
+import argparse
+import os
+import sys
+from pathlib import Path
+
+import torch
+import torch.backends.cudnn as cudnn
+
+from models.common import DetectMultiBackend
+from utils.datasets import IMG_FORMATS, VID_FORMATS, LoadImages, LoadStreams
+from utils.general import (LOGGER, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
+                           increment_path, non_max_suppression, scale_coords, strip_optimizer, xyxy2xywh)
+from utils.plots import Annotator, colors, save_one_box
+from utils.torch_utils import select_device, time_sync
+from detection import detect
+from config import parse_args
+
+@torch.no_grad()
+
+
+def main(opt):
+    check_requirements(exclude=('tensorboard', 'thop'))
+    images, bboxes, confs = detect.run(**vars(opt))
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
